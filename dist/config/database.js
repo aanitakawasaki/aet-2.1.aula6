cd1c53e152d2eea1b9b0d3cc4d33631dfdaefb74
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2024-05-26 19:16:43 -0300

    commit

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { Pool } from 'pg';
-const poolConfig = {
+import pg from 'pg';
+const { Pool } = pg;
+const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: Number(process.env.DB_PORT) || 5432
-};
-const pool = new Pool(poolConfig);
+});
 pool.on('error', (err) => {
     console.error(`Erro inesperado no cliente da pool: ${err}`);
     process.exit(-1);
